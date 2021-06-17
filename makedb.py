5fb8f761fa01c4eafa552bf08dd53f1146b00005
aniketjana03 <79072670+aniketjana03@users.noreply.github.com>
2021-06-17 19:31:48 +0530

    SQL connection made

--- makedb.py
+++ makedb.py
@@ -5,9 +5,9 @@ from boto.s3.connection import S3Connection
 
 class MakeDB:
     def __init__(self):
-        host = os.environ.get('server')
-        user = os.environ.get('SQLusername')
-        password = os.environ.get('SQLpassword')
+        host = os.environ.get('127.0.0.1')
+        user = os.environ.get('root')
+        password = os.environ.get('root')
 
         stmts = parse_sql('final.sql')
         self.con = pymysql.connect(host=host, user=user, password=password,
